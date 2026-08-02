f3652ff802c706ad508e48a21885b40e69999d51
Vishvesh Arvindrao Paturkar <130577681+vishuhack@users.noreply.github.com>
2026-08-02 19:17:12 +0530

    Time: 0 ms (100%), Space: 15.1 MB (69.03%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,33 +1,51 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
+
         int n = nums.size();
+
         int low = 0;
-        int high = nums.size() - 1;
+        int high = n - 1;
 
         while (low <= high) {
-            int mid = (low + high) / 2;
+
+            int mid = low + (high - low) / 2;
+
+            // Target found
             if (nums[mid] == target) {
                 return mid;
             }
 
-            // Check Which half is sorted
+            // Check which half is sorted
+
+            // Left half is sorted
             if (nums[low] <= nums[mid]) {
-                // Left half sorted
-                // Chack terget lies in this half
-                if (target >= nums[low] && target <= nums[mid]) {
+
+                // Check if target lies in the left half
+                if (target >= nums[low] && target < nums[mid]) {
                     high = mid - 1;
-                } else {
+                }
+                else {
+                    // Search in the right half
                     low = mid + 1;
                 }
-            } else {
-                if (target >= nums[mid] && target <= nums[high]) {
+            }
+
+            // Right half is sorted
+            else {
+
+                // Check if target lies in the right half
+                if (target > nums[mid] && target <= nums[high]) {
                     low = mid + 1;
-                } else {
+                }
+                else {
+                    // Search in the left half
                     high = mid - 1;
                 }
             }
         }
+
+        // Target not found
         return -1;
     }
 };
