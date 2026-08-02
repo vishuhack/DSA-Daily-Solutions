d1d1932725046b45dc82bc63320df02c9a83a43c
Vishvesh Arvindrao Paturkar <130577681+vishuhack@users.noreply.github.com>
2026-08-02 16:25:53 +0530

    Time: 0 ms (100%), Space: 28.1 MB (92.84%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -2,28 +2,54 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         int n = nums.size();
+
+        // Stores the current majority candidate
         int candidate = nums[0];
+
+        // Vote count for the current candidate
         int cnt = 0;
 
-        for(int i = 0; i < nums.size(); i++){
-            if(cnt == 0){
+        // -------------------------------
+        // 1st Pass : Boyer-Moore Voting
+        // -------------------------------
+        // Idea:
+        // Different elements cancel each other's votes.
+        // If count becomes 0, choose the current element as the new candidate.
+        for (int i = 0; i < n; i++) {
+
+            // No active candidate
+            if (cnt == 0) {
                 candidate = nums[i];
                 cnt = 1;
             }
-            else if(candidate == nums[i]){
+
+            // Same as candidate -> increase vote
+            else if (candidate == nums[i]) {
                 cnt++;
             }
-            else{
+
+            // Different element -> cancel one vote
+            else {
                 cnt--;
             }
         }
+
+        // ----------------------------------------
+        // 2nd Pass : Verify the candidate
+        // ----------------------------------------
+        // (LeetCode guarantees majority exists,
+        // but verification is useful in general.)
         cnt = 0;
-        for(int i = 0; i < nums.size(); i++){
-            if(candidate == nums[i]){
+
+        for (int num : nums) {
+            if (num == candidate)
                 cnt++;
-            }
         }
-        if(cnt > n/2) return candidate;
+
+        // Check if candidate occurs more than n/2 times
+        if (cnt > n / 2)
+            return candidate;
+
         return 0;
     }
 };
