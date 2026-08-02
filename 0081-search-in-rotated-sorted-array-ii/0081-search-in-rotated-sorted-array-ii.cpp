1c847d5797733bd4b3ff3af54c4d9cda3ab0a6f7
Vishvesh Arvindrao Paturkar <130577681+vishuhack@users.noreply.github.com>
2026-08-02 19:28:34 +0530

    Time: 0 ms (100%), Space: 18.1 MB (8.47%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,35 +1,53 @@
 class Solution {
 public:
     bool search(vector<int>& arr, int k) {
-        int low= 0;
+        int low = 0;
         int high = arr.size() - 1;
 
-        while(low <= high){
-            int mid = (low + high) / 2;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
 
-            if(arr[mid] == k) return true;
+            // Target found
+            if (arr[mid] == k) {
+                return true;
+            }
 
-            if(arr[low] == arr[mid] && arr[mid] == arr[high]){
+            // Duplicates make it difficult to identify
+            // which half of the array is sorted.
+            if (arr[low] == arr[mid] && arr[mid] == arr[high]) {
                 low++;
                 high--;
                 continue;
             }
 
-            if(arr[low] <= arr[mid]){
-                if(arr[low] <= k && k <= arr[mid]){
+            // Left half is sorted
+            if (arr[low] <= arr[mid]) {
+
+                // Target lies in the sorted left half
+                if (arr[low] <= k && k < arr[mid]) {
                     high = mid - 1;
-                }else{
-                    low = mid + 1;
                 }
-            }else{
-                if(arr[mid] <= k && k <= arr[high]){
+                else {
+                    // Search in the right half
                     low = mid + 1;
-                }else{
-                    high = mid - 1;
                 }
             }
 
+            // Right half is sorted
+            else {
+
+                // Target lies in the sorted right half
+                if (arr[mid] < k && k <= arr[high]) {
+                    low = mid + 1;
+                }
+                else {
+                    // Search in the left half
+                    high = mid - 1;
+                }
+            }
         }
+
+        // Target is not present
         return false;
     }
 };
