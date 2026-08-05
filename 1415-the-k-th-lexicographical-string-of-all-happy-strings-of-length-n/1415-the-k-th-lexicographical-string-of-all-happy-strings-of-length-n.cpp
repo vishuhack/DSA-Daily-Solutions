a293729c22973da585efdb191de57eb7d8c9ce85
Vishvesh Arvindrao Paturkar <130577681+vishuhack@users.noreply.github.com>
2026-08-05 21:19:55 +0530

    Time: 4 ms (50.21%), Space: 8 MB (74.93%) - LeetHub

--- 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     int cnt;
-    void solve(int n, vector<int>& used, vector<string>& result, string& curr, int k){
+    string result = "";
+    void solve(int n, vector<int>& used, string& curr, int k){
         if(curr.length() == n){
             cnt++;
             if(cnt == k)
-                result.push_back(curr);
+                result = curr;
             return;
         }
 
         for(char ch = 'a'; ch <= 'c' ; ch++){
             if(curr.empty() || curr.back() != ch){
                 curr.push_back(ch); // Do
-                solve(n, used, result, curr, k); // Explore
+                solve(n, used, curr, k); // Explore
                 curr.pop_back(); //Undo
 
             }
@@ -20,10 +21,10 @@ public:
     }
     string getHappyString(int n, int k) {
         vector<int> used(3,-1);
-        vector<string> result;
+        
         string curr = "";
-        solve(n, used, result, curr, k);
-        if (result.empty()) return "";
-        return result[0];
+        solve(n, used, curr, k);
+        // if (result == NULL) return "";
+        return result;
     }
 };
