e30bf7464300b6c5e013c4633223370cbde572c3
Vishvesh Arvindrao Paturkar <130577681+vishuhack@users.noreply.github.com>
2026-08-06 20:02:26 +0530

    Time: 867 ms (10.51%), Space: 10.6 MB (96.01%) - LeetHub

---
+++ 2014-longest-subsequence-repeated-k-times/2014-longest-subsequence-repeated-k-times.cpp
@@ -0,0 +1,68 @@
+class Solution {
+public:
+    string result;
+    bool isSubsequence(string& s, string& curr, int k) {
+        int i = 0;
+        int j = 0;
+        int L = curr.length();
+        int n = s.length();
+
+        while(i < n && j < k*L ){
+            if(s[i] == curr[j%L]){
+                j++;
+            }
+            i++;
+        }
+        return j == k*L;
+    }
+    void solve(string& s, string& curr, vector<bool>& canUse,
+               vector<int>& requiredFreq, int k, int maxLen) {
+        if (curr.length() > maxLen) {
+            return;
+        }
+        if (((curr.length() > result.length()) ||
+            (curr.length() == result.length() && curr > result)) &&
+                isSubsequence(s, curr, k)) {
+            result = curr;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (canUse[i] == false || requiredFreq[i] == 0) {
+                continue;
+            }
+
+            char ch = i + 'a';
+            curr.push_back(ch);
+            requiredFreq[i]--;
+
+            solve(s, curr, canUse, requiredFreq, k, maxLen);
+
+            curr.pop_back();
+            requiredFreq[i]++;
+        }
+    }
+    string longestSubsequenceRepeatedK(string s, int k) {
+        int n = s.length();
+
+        int freq[26] = {};
+        for (char& ch : s) {
+            freq[ch - 'a']++;
+        }
+
+        vector<bool> canUse(26, false);
+        vector<int> requiredFreq(26, 0);
+
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] >= k) {
+                canUse[i] = true;
+                requiredFreq[i] = freq[i] / k;
+            }
+        }
+        int maxLen = n / k;
+        string curr;
+
+        solve(s, curr, canUse, requiredFreq, k, maxLen);
+
+        return result;
+    }
+};
