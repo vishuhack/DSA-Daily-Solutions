106e3f0e6bdba889c90ad8b5f99ef4ed381148c8
Vishvesh Arvindrao Paturkar <130577681+vishuhack@users.noreply.github.com>
2026-08-08 15:16:15 +0530

    Time: 3 ms (11.64%), Space: 22.7 MB (53.72%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,17 +1,36 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int cnt= 0;
-        vector<int> expectedNums;
-        for(int i = 0; i < nums.size(); i++){
-            if(i == 0 || nums[i] != nums[i-1]){
-                cnt++;
-                expectedNums.push_back(nums[i]);
+
+        // Appraoch 1 : without two pointers
+        // int cnt= 0;
+        // vector<int> expectedNums;
+        // for(int i = 0; i < nums.size(); i++){
+        //     if(i == 0 || nums[i] != nums[i-1]){
+        //         cnt++;
+        //         expectedNums.push_back(nums[i]);
+        //     }
+        // }
+        // for(int i = 0; i < expectedNums.size(); i++){
+        //     nums[i] = expectedNums[i];
+        // }
+        // return cnt;
+
+        //Approach 2 : Two pointers
+        int i = 0;
+        int j = i+1;
+        int n = nums.size();
+
+        while(j < n){
+            if(nums[i] == nums[j]){
+                j++;
+            }
+            else if(nums[i] != nums[j]){
+                i++;
+                nums[i] = nums[j];
+                j++;
             }
         }
-        for(int i = 0; i < expectedNums.size(); i++){
-            nums[i] = expectedNums[i];
-        }
-        return cnt;
+        return i+1;
     }
 };
