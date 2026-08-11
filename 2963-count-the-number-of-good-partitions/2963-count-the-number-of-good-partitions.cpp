44075e0c7c8363a04e0be3075427bbe962fb0809
Vishvesh Arvindrao Paturkar <130577681+vishveshpaturkar@users.noreply.github.com>
2026-08-11 09:01:38 +0530

    Time: 94 ms (69.3%), Space: 138.6 MB (92.98%) - LeetHub

--- 2963-count-the-number-of-good-partitions/2963-count-the-number-of-good-partitions.cpp
+++ 2963-count-the-number-of-good-partitions/2963-count-the-number-of-good-partitions.cpp
@@ -3,19 +3,31 @@ public:
     int numberOfGoodPartitions(vector<int>& nums) {
         int M = 1e9 + 7;
         int n = nums.size();
+
+        // Store the last occurrence of every number
         unordered_map<int, int> mp;
-        for (int k = 0; k < nums.size(); k++)
-            mp[nums[k]] = k;
+        for (int i = 0; i < n; i++) {
+            mp[nums[i]] = i;
+        }
 
         int i = 0;
+
+        // Rightmost index till which current group must extend
         int j = mp[nums[0]];
+
         int result = 1;
 
         while (i < n) {
+
+            // Previous group is completely finished
+            // So we found a new valid partition boundary
             if (i > j) {
-                result = (result * 2)%M;
+                result = (result * 2) % M;
             }
+
+            // Extend j if current element appears later
             j = max(j, mp[nums[i]]);
+
             i++;
         }
 
