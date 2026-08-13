7b216f6d3d1a43e803cdeb8110b06ecf764f4424
Vishvesh Arvindrao Paturkar <130577681+vishveshpaturkar@users.noreply.github.com>
2026-08-13 07:56:22 +0530

    Time: 1846 ms (75.37%), Space: 530.5 MB (31.18%) - LeetHub

--- 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.cpp
+++ 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.cpp
@@ -1,24 +1,45 @@
 class Solution {
 public:
     int longestBalanced(vector<int>& nums) {
+        // int maxL = 0;
+        // int n = nums.size();
+        // for (int i = 0; i < n; i++) {
+        //     int even = 0;
+        //     int odd = 0;
+        //     unordered_set<int> st;
+        //     for (int j = i; j < n; j++) {
+        //         if (st.find(nums[j]) == st.end()) {
+        //             if (nums[j] % 2 == 0) {
+        //                 even++;
+        //             } else {
+        //                 odd++;
+        //             }
+        //             st.insert(nums[j]);
+
+        //         }
+        //         if(even == odd){
+        //             maxL = max(maxL, j-i+1);
+        //         }
+        //     }
+        // }
+        // return maxL;
+
+        // Approach 2
+
         int maxL = 0;
         int n = nums.size();
         for (int i = 0; i < n; i++) {
-            int even = 0;
-            int odd = 0;
-            unordered_set<int> st;
+            unordered_set<int> even;
+            unordered_set<int> odd;
             for (int j = i; j < n; j++) {
-                if (st.find(nums[j]) == st.end()) {
-                    if (nums[j] % 2 == 0) {
-                        even++;
-                    } else {
-                        odd++;
-                    }
-                    st.insert(nums[j]);
-                    
+                if (nums[j] % 2 == 0) {
+                    even.insert(nums[j]);
+                } else {
+                    odd.insert(nums[j]);
                 }
-                if(even == odd){
-                    maxL = max(maxL, j-i+1);
+
+                if (even.size() == odd.size()) {
+                    maxL = max(maxL, j - i + 1);
                 }
             }
         }
