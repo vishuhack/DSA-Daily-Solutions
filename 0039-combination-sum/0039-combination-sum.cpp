fb67d87b810f4529e92d65a05e7c90fa0e08bc7b
Vishvesh Arvindrao Paturkar <130577681+vishveshpaturkar@users.noreply.github.com>
2026-08-16 23:49:19 +0530

    Time: 3 ms (51.97%), Space: 14.1 MB (77.39%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void findS(int idx, int target, vector<int>& candidates, vector<int>& ds, vector<vector<int>>& ans){
+        if(idx == candidates.size()){
+            if(target == 0){
+                ans.push_back(ds);
+            }
+            return;
+        }
+
+        //Check id target <= current element
+        if(candidates[idx] <= target){
+            ds.push_back(candidates[idx]);
+            findS(idx, target - candidates[idx], candidates, ds, ans);
+            ds.pop_back();
+        }
+        findS(idx+1, target , candidates, ds, ans);
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> ds;
+        findS(0, target, candidates, ds, ans);
+        return ans;
+    }
+};
